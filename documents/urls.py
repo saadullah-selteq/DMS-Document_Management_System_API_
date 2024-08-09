6a35310b5e3b243827b95abf9794053aa7e41631
saadullah <saadu@selteq.net>
2024-08-08 23:31:37 -0700

    update urls

--- documents/urls.py
+++ documents/urls.py
@@ -1,12 +1,10 @@
-# documents/urls.py
-
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import BusinessViewSet, SubBusinessViewSet, FolderViewSet, FileViewSet
 
 router = DefaultRouter()
 router.register(r'businesses', BusinessViewSet)
-router.register(r'sub_businesses', SubBusinessViewSet)
+router.register(r'sub-businesses', SubBusinessViewSet)
 router.register(r'folders', FolderViewSet)
 router.register(r'files', FileViewSet)
 
